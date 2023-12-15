dfc7be40aeebbb856290a875d40565a2d6e69f4d
amichelo <andrea.michelotti@lnf.infn.it>
2023-12-15 12:41:07 +0100

    building up sparc epics opis

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+phoebus -settings $OPIHOME/settings.ini -resource $OPIHOME/phoebus_mrf_interface/MRFIOC_EVG.bob -resource $OPIHOME/phoebus_mrf_interface/MRFIOC_EVR0.bob -resource $OPIHOME/phoebus_mrf_interface/MRFIOC_EVR1.bob
