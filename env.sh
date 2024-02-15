951e11ad0e2f431063536736df9a8de70776eea7
amichelo <andrea.michelotti@lnf.infn.it>
2024-02-15 10:10:52 +0100

    added env to start

---
+++ env.sh
@@ -0,0 +1,18 @@
+export EPICS_CA_ADDR_LIST="plsparcmrf001.lnf.infn.it \
+    bdsparcplxbpm007 \
+    bdsparcplxbpm001 \
+    bdsparcac3bpm001 \
+    bdsparcptlbpm001 \
+    bdsparcutlbpm001 \
+    bdsparcdlgbpm001 \
+    bdsparcdlgbpm002 \
+    bdsparcdlgbpm003 \
+    bdsparcplxbpm002 \
+    bdsparcplxbpm003 \
+    bdsparcplxbpm004 \
+    bdsparcplxbpm005 \
+    bdsparcplxbpm006 \
+    rdsparcpitaia001.lnf.infn.it"
+
+export OPIHOME=`pwd`
+export EPICS_ARCHIVER=epics-archiver.apps.okd-datest.lnf.infn.it
--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 ## current directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ARCH=`uname -s`
+
+PHOEBUS="phoebus"
+
+if [ "$ARCH" == "Darwin" ];then
+    if [ -f /Applications/CSS_Phoebus.app/phoebus-4.7.3-SNAPSHOT/product-4.7.3-SNAPSHOT.jar ]; then 
+	PHOEBUS="java -jar /Applications/CSS_Phoebus.app/phoebus-4.7.3-SNAPSHOT/product-4.7.3-SNAPSHOT.jar"
+    fi
+fi
 
 if [ -z "$OPIHOME" ]; then
     export OPIHOME=$DIR
@@ -9,10 +18,12 @@ if [ -z "$OPIDATA" ]; then
     export OPIDATA=$DIR
 fi
 ## test if phoebus executable is found in path
-if ! command -v phoebus &> /dev/null
+if ! command -v $PHOEBUS &> /dev/null
 then
-    echo "phoebus executable not found in PATH"
-    exit
+     echo "# phoebus executable not found in PATH nor alias"
+     exit
+
+    
 fi
 if [ ! -f "$OPIHOME/.setting_applied" ]; then
     if [ -n "$EPICS_CA_ADDR_LIST" ]; then
@@ -38,4 +49,4 @@ if [ ! -f "$OPIHOME/.setting_applied" ]; then
     
 fi
 
-phoebus -settings $OPIHOME/settings.ini -resource $OPIHOME/Launcher.bob
+$PHOEBUS -settings $OPIHOME/settings.ini -resource $OPIHOME/Launcher.bob
