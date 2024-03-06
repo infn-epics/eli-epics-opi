ab81cf2814f7027de3fa2d1d1f9a505b584ce535
amichelo <andrea.michelotti@lnf.infn.it>
2024-03-06 23:39:08 +0100

    update start

--- start.sh
+++ start.sh
@@ -46,10 +46,6 @@ if [ ! -f "$OPIHOME/.setting_applied" ]; then
         touch "$OPIHOME/.setting_applied"
     fi
 
-    if [ -n "$PHOEBUS_SAVE_AND_RESTORE" ]; then
-        echo "org.phoebus.applications.saveandrestore.client/jmasar.service.url=http://$PHOEBUS_SAVE_AND_RESTORE:8080/save-restore" >> $OPIHOME/settings.ini
-        touch "$OPIHOME/.setting_applied"
-    fi
     if [ -n "$PHOEBUS_SCAN_SERVER" ]; then
         echo "org.csstudio.scan.client/host=$PHOEBUS_SCAN_SERVER" >> $OPIHOME/settings.ini
         echo "org.csstudio.scan.client/port=4810" >> $OPIHOME/settings.ini
