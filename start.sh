2a4872ae15d0daeb536821cf125bc1715c0a9d4f
amichelo <andrea.michelotti@lnf.infn.it>
2024-03-26 17:32:15 +0100

    if PHOEBUSINI SET use it

--- start.sh
+++ start.sh
@@ -25,57 +25,64 @@ then
 
     
 fi
-if [ ! -f "$OPIHOME/.setting_applied" ]; then
-    if [ -n "$EPICS_CA_ADDR_LIST" ]; then
-        # Apply the modification
-        echo "org.phoebus.pv.ca/addr_list=$EPICS_CA_ADDR_LIST" >> "$OPIHOME/settings.ini"
 
-        # Create a marker file to indicate that the modification has been applied
-        touch "$OPIHOME/.setting_applied"
-    fi
-    if [ -n "$EPICS_ARCHIVER" ]; then
-        echo "org.csstudio.trends.databrowser3/urls=pbraw://$EPICS_ARCHIVER/retrieval" >> $OPIHOME/settings.ini
-        echo "org.csstudio.trends.databrowser3/archives=pbraw://$EPICS_ARCHIVER/retrieval" >> $OPIHOME/settings.ini
+if [ -n "$PHOEBUSINI" ];then
+    echo "* USING $PHOEBUSINI
+    $PHOEBUS -settings $PHOEBUSINI -resource $OPIHOME/Launcher.bob
 
-        touch "$OPIHOME/.setting_applied"
+else
+    if [ ! -f "$OPIHOME/.setting_applied" ]; then
+        if [ -n "$EPICS_CA_ADDR_LIST" ]; then
+            # Apply the modification
+            echo "org.phoebus.pv.ca/addr_list=$EPICS_CA_ADDR_LIST" >> "$OPIHOME/settings.ini"
 
-    fi
+            # Create a marker file to indicate that the modification has been applied
+            touch "$OPIHOME/.setting_applied"
+        fi
+        if [ -n "$EPICS_ARCHIVER" ]; then
+            echo "org.csstudio.trends.databrowser3/urls=pbraw://$EPICS_ARCHIVER/retrieval" >> $OPIHOME/settings.ini
+            echo "org.csstudio.trends.databrowser3/archives=pbraw://$EPICS_ARCHIVER/retrieval" >> $OPIHOME/settings.ini
 
-    if [ -n "$CHANNEL_FINDER" ]; then
-        echo "org.phoebus.channelfinder/channelfinder.serviceURL=http://$CHANNEL_FINDER/ChannelFinder" >> $OPIHOME/settings.ini
-        touch "$OPIHOME/.setting_applied"
-    fi
+            touch "$OPIHOME/.setting_applied"
 
-    if [ -n "$PHOEBUS_SCAN_SERVER" ]; then
-        echo "org.csstudio.scan.client/host=$PHOEBUS_SCAN_SERVER" >> $OPIHOME/settings.ini
-        echo "org.csstudio.scan.client/port=4810" >> $OPIHOME/settings.ini
+        fi
 
-    fi
-    if [ -n "$PHOEBUS_SAVE_AND_RESTORE" ]; then
-        echo "* PHOEBUS_SAVE_AND_RESTORE= $PHOEBUS_SAVE_AND_RESTORE"
+        if [ -n "$CHANNEL_FINDER" ]; then
+            echo "org.phoebus.channelfinder/channelfinder.serviceURL=http://$CHANNEL_FINDER/ChannelFinder" >> $OPIHOME/settings.ini
+            touch "$OPIHOME/.setting_applied"
+        fi
 
-        echo "org.phoebus.applications.saveandrestore.client/jmasar.service.url=http://$PHOEBUS_SAVE_AND_RESTORE/save-restore" >> $OPIHOME/settings.ini
-        touch "$OPIHOME/.setting_applied"
-    fi
-    if [ -n "$PHOEBUS_SCAN_SERVER" ]; then
-        echo "* PHOEBUS_SCAN_SERVER= $PHOEBUS_SCAN_SERVER"
+        if [ -n "$PHOEBUS_SCAN_SERVER" ]; then
+            echo "org.csstudio.scan.client/host=$PHOEBUS_SCAN_SERVER" >> $OPIHOME/settings.ini
+            echo "org.csstudio.scan.client/port=4810" >> $OPIHOME/settings.ini
 
-        echo "org.csstudio.scan.client/host=$PHOEBUS_SCAN_SERVER" >> $OPIHOME/settings.ini
-        echo "org.csstudio.scan.client/port=4810" >> $OPIHOME/settings.ini
-        touch "$OPIHOME/.setting_applied"
+        fi
+        if [ -n "$PHOEBUS_SAVE_AND_RESTORE" ]; then
+            echo "* PHOEBUS_SAVE_AND_RESTORE= $PHOEBUS_SAVE_AND_RESTORE"
+
+            echo "org.phoebus.applications.saveandrestore.client/jmasar.service.url=http://$PHOEBUS_SAVE_AND_RESTORE/save-restore" >> $OPIHOME/settings.ini
+            touch "$OPIHOME/.setting_applied"
+        fi
+        if [ -n "$PHOEBUS_SCAN_SERVER" ]; then
+            echo "* PHOEBUS_SCAN_SERVER= $PHOEBUS_SCAN_SERVER"
+
+            echo "org.csstudio.scan.client/host=$PHOEBUS_SCAN_SERVER" >> $OPIHOME/settings.ini
+            echo "org.csstudio.scan.client/port=4810" >> $OPIHOME/settings.ini
+            touch "$OPIHOME/.setting_applied"
 
 
-    fi
-    if [ -n "$PHOEBUS_OLOG" ]; then
-        echo "* PHOEBUS_OLOG= $PHOEBUS_OLOG"    
-        echo "org.phoebus.olog.es.api/olog_url=http://$PHOEBUS_OLOG/Olog" >> $OPIHOME/settings.ini
-        echo "org.phoebus.olog.api/olog_url=http://$PHOEBUS_OLOG/Olog" >> $OPIHOME/settings.ini
-        echo "org.phoebus.logbook/logbook_factory=olog-es" >> $OPIHOME/settings.ini
-        echo "org.phoebus.olog.api/username=epics" >> $OPIHOME/settings.ini
-        echo "org.phoebus.olog.api/password=epics" >> $OPIHOME/settings.ini
-        touch "$OPIHOME/.setting_applied"
-    fi
+        fi
+        if [ -n "$PHOEBUS_OLOG" ]; then
+            echo "* PHOEBUS_OLOG= $PHOEBUS_OLOG"    
+            echo "org.phoebus.olog.es.api/olog_url=http://$PHOEBUS_OLOG/Olog" >> $OPIHOME/settings.ini
+            echo "org.phoebus.olog.api/olog_url=http://$PHOEBUS_OLOG/Olog" >> $OPIHOME/settings.ini
+            echo "org.phoebus.logbook/logbook_factory=olog-es" >> $OPIHOME/settings.ini
+            echo "org.phoebus.olog.api/username=epics" >> $OPIHOME/settings.ini
+            echo "org.phoebus.olog.api/password=epics" >> $OPIHOME/settings.ini
+            touch "$OPIHOME/.setting_applied"
+        fi
     
-fi
+    fi
 
-$PHOEBUS -settings $OPIHOME/settings.ini -resource $OPIHOME/Launcher.bob
+    $PHOEBUS -settings $OPIHOME/settings.ini -resource $OPIHOME/Launcher.bob
+fi
