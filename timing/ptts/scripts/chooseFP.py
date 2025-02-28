42977f51eb2c6fe04c5f5e5e54400a891c9f6e2a
amichelo <andrea.michelotti@lnf.infn.it>
2025-02-28 12:12:48 +0100

    moved out ptts now is mrf230-opi submodule

--- timing/ptts/scripts/chooseFP.py
+++
@@ -1,9 +0,0 @@
-from org.csstudio.display.builder.runtime.script import PVUtil
-from org.csstudio.display.builder.runtime.script import ScriptUtil
-
-logger = ScriptUtil.getLogger()
-pv = PVUtil.getLong(pvs[0])
-logger.info("PV Name " + str(pvs[0]) + " Val:"+str(pv))
-widget.getPropertyValue("macros").add("FP", str(pv))
-widget.setPropertyValue("file","")
-widget.setPropertyValue("file","Timing_Output.bob")
